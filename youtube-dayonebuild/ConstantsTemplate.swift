18543f78ba2db606437511c072f62e9eaca99b55
Ashiqur Rahman <ashiqseven@icloud.com>
2021-11-06 02:20:06 +0600

    modified gitignore to stop tracking constant.swift, added ConstantsTemplate.swift instead

---
+++ youtube-dayonebuild/ConstantsTemplate.swift
@@ -0,0 +1,17 @@
+//
+//  constant.swift
+//  youtube-dayonebuild
+//
+//  Created by ASHIQUR RAHMAN on 11/5/21.
+//
+
+import Foundation
+
+/*
+struct Constants {
+    
+    static var API_KEY = ""
+    static var PLAYLIST_ID = ""
+    static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+}
+*/
---
+++ youtube-dayonebuild/video.swift
@@ -0,0 +1,17 @@
+//
+//  video.swift
+//  youtube-dayonebuild
+//
+//  Created by ASHIQUR RAHMAN on 11/5/21.
+//
+
+import Foundation
+
+struct Video {
+    
+    var videoId = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+}
